2ca48263283907fb8e9bb4aca487094336613583
flamingsharad <sharad1016643501@gmail.com>
2024-05-03 15:27:15 +0530

    speed

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar/Navbar";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
             <Navbar />
+            <Analytics/>
+            <SpeedInsights/>
             {children}
       </body>
     </html>
